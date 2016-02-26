edab4457348188726603d0deb2e962b92e78d05a
Michael Rodov <rodov@hp.com>
2016-02-26 22:00:18 +0200

    ui

---
+++ js/data.js
@@ -0,0 +1,89 @@
+/**
+ * Created by Carlos on 22/02/2016.
+ */
+
+var mock = {
+    headers: ['player','kills','deaths'],
+    games: {
+        game1: {
+            type: "Death Match",
+            length: "20",
+            players: {
+                RODOV: {
+                    kills: 11,
+                    deaths: 10
+                },
+                ACE: {
+                    kills: 43,
+                    deaths: 1
+                },
+                MAMBA: {
+                    kills: 22,
+                    deaths: 10
+                },
+                player4: {
+                    kills: 19,
+                    deaths: 70
+                },
+                player5: {
+                    kills: 23,
+                    deaths: 9
+                }
+            }
+        },
+        game2: {
+            type: "Capture The Flag",
+            length: "15",
+            players: {
+
+                player1: {
+                    kills: 11,
+                    deaths: 10
+                },
+                player2: {
+                    kills: 43,
+                    deaths: 1
+                },
+                player3: {
+                    kills: 22,
+                    deaths: 10
+                },
+                player4: {
+                    kills: 15,
+                    deaths: 29
+                },
+                player5: {
+                    kills: 23,
+                    deaths: 9
+                }
+            }
+        },
+        game3: {
+            type: "Capture The Flag",
+            length: "20",
+            players: {
+
+                player1: {
+                    kills: 11,
+                    deaths: 10
+                },
+                player2: {
+                    kills: 43,
+                    deaths: 1
+                },
+                player3: {
+                    kills: 22,
+                    deaths: 10
+                },
+                player4: {
+                    kills: 15,
+                    deaths: 29
+                },
+                player5: {
+                    kills: 23,
+                    deaths: 9
+                }
+            }
+        }
+    }
+};
